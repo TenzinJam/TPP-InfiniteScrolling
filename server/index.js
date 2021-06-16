fb68dcb35358e79f604f5bef10b0c6b97c6759ef
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-16 17:16:39 -0400

    setting up server

---
+++ server/index.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const data = require('./data')
+const PORT = process.env.PORT || 8080
+const app = express()
+module.exports = app
+
+const createApp = () => {
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+  app.use('/api', require('./api'))
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
+
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  const server = app.listen(PORT, () =>
+    console.log(`Listening on port ${PORT}`)
+  )
+}
+
+const syncDb = () => db.sync()
+
+async function bootApp() {
+  await sessionStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
