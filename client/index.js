c7aa892364d1216de7add8f5ce4bc297a4467233
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-16 17:36:12 -0400

    setting up the components for scrolling

---
+++ client/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import axios from "axios"
+
+class Scroll extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      photo: [],
+      pinner: "",
+      likeCount: null,
+      commentCount: null,
+      loading: false,
+      page: 0,
+      previousPosition: 0
+    }
+  }
+  render(){
+
+    
+    return (
+
+    );
+  }
+}
