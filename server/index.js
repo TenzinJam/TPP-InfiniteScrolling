46f2274bf9cb76e50149c433e7123343bbfde3d2
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-17 13:04:10 -0400

    tweeking server and getting rid of middlewares I won't need

--- server/index.js
+++ server/index.js
@@ -43,15 +43,10 @@ const startListening = () => {
 const syncDb = () => db.sync()
 
 async function bootApp() {
-  await sessionStore.sync()
-  await syncDb()
   await createApp()
   await startListening()
 }
-// This evaluates as true when this file is run directly from the command line,
-// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
-// It will evaluate false when this module is required by another module - for example,
-// if we wanted to require our app in a test spec
+
 if (require.main === module) {
   bootApp()
 } else {
